fff4bc9de477e36282ccefdd4169f545bb8b7922
main0crine-oss <main0crine@gmail.com>
2025-10-29 00:28:53 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,3 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ base:'/Wheelbot-webapp/', plugins:[react()], server:{port:5173}, build:{outDir:'dist'} })
